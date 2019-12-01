3eee2ba64b20f145573f16363f3247a83e2a35ed
Yichun Wu <kobo1998@live.com>
2019-12-01 01:34:31 -0500

    Milestone4: Fix a bug

--- routers/follow.js
+++ routers/follow.js
@@ -12,82 +12,83 @@ router.post('/',jsonParser,function(req,res){
             'error': 'req.body.username is null'
         })
     }
-   
-    if(req.body.follow == null){
-        req.body.follow = true
-    }
-    var db = req.app.locals.db
-    db.collection("users").find({'username':req.body.username}).toArray(function(err, result){
-        if(err){
-            res.status(500).json({
-                'status': 'error',
-                'error':err
-            })
+    else{
+        if(req.body.follow == null){
+            req.body.follow = true
         }
-        else if(result.length<=0){
-            res.status(400).json({
-                'status': 'error',
-                'error':'User does not exist'
-            })
-        }
-        else{
-            
-            if(req.body.follow){
-                db.collection("follow").find({'following': req.body.username, 'follower': req.session.user}).toArray(function(err, result){
-                    if(err){
-                        res.status(500).json({
-                            'status': 'error',
-                            'error':err
-                        })
-                    }
-                    else if(result.length>0){
-                        res.status(200).json({
-                            'status': 'error',
-                            'error':'Already followed'
-                        })
-                    }
-                    else{
-                        db.collection("follow").insertOne({'following': req.body.username, 'follower': req.session.user}, function(err, result){
-                            if(err){
-                                json = {
-                                    'status': "error",
-                                    'error': err
-                                }
-                                res.status(500).json(json)
-                            }
-                            else{
-                                json = {'status': "OK"}
-                                res.status(200).json(json)
-                            }
-                        })
-                    }            
-        
+        var db = req.app.locals.db
+        db.collection("users").find({'username':req.body.username}).toArray(function(err, result){
+            if(err){
+                res.status(500).json({
+                    'status': 'error',
+                    'error':err
+                })
+            }
+            else if(result.length<=0){
+                res.status(400).json({
+                    'status': 'error',
+                    'error':'User does not exist'
                 })
             }
             else{
-                
-                db.collection("follow").remove({'following': req.body.username, 'follower': req.session.user}, function(err, obj){
-                    if(err){
-                        res.status(500).json({
-                            status:"error",
-                            error:err
-                        });
-                    }
-                    else if(obj.result.n <= 0){
-                        res.status(400).json({
-                            status:"error",
-                            error:"Not followed"
-                        });
-                    }
-                    else{
-                        res.status(200).json({
-                            status:"OK"
-                        });
-                    }
-                })
+                if(req.body.follow){
+                    db.collection("follow").find({'following': req.body.username, 'follower': req.session.user}).toArray(function(err, result){
+                        if(err){
+                            res.status(500).json({
+                                'status': 'error',
+                                'error':err
+                            })
+                        }
+                        else if(result.length>0){
+                            res.status(200).json({
+                                'status': 'error',
+                                'error':'Already followed'
+                            })
+                        }
+                        else{
+                            db.collection("follow").insertOne({'following': req.body.username, 'follower': req.session.user}, function(err, result){
+                                if(err){
+                                    json = {
+                                        'status': "error",
+                                        'error': err
+                                    }
+                                    res.status(500).json(json)
+                                }
+                                else{
+                                    json = {'status': "OK"}
+                                    res.status(200).json(json)
+                                }
+                            })
+                        }            
+            
+                    })
+                }
+                else{
+                    
+                    db.collection("follow").remove({'following': req.body.username, 'follower': req.session.user}, function(err, obj){
+                        if(err){
+                            res.status(500).json({
+                                status:"error",
+                                error:err
+                            });
+                        }
+                        else if(obj.result.n <= 0){
+                            res.status(400).json({
+                                status:"error",
+                                error:"Not followed"
+                            });
+                        }
+                        else{
+                            res.status(200).json({
+                                status:"OK"
+                            });
+                        }
+                    })
+                }
             }
-        }
-    })
+        })
+    }
+    
 
 })
 
