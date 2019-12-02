dcbdeac2e090ab18720145b32961d6a97d6a3142
Yichun Wu <kobo1998@live.com>
2019-12-02 17:36:35 -0500

    Update search.js

--- routers/search.js
+++ routers/search.js
@@ -15,86 +15,6 @@ router.post('/',(req,res)=>{
             error:"Login First"
         });
     }
-    // req.body.default = true
-    // //Default values
-    // req.body.current = Date.now()
-    // if(req.body.timestamp == null || req.body.timestamp == '' || req.body.timestamp <= 0){
-    //     req.body.timestamp = req.body.current
-    // }
-    // else{
-    //     req.body.default = false
-    // }
-    // if(req.body.limit == null || req.body.limit == '' ||parseInt(req.body.limit) <= 0){
-    //     req.body.limit = 25
-    // }
-    // else if(parseInt(req.body.limit) >= 100){
-    //     req.body.limit = 100
-    //     req.body.default = false
-    // }
-    // else{
-    //     req.body.default = false
-    // }
-    // if(req.body.rank == null || req.body.rank != 'time'){
-    //     req.body.rank = 'interest'
-    // }
-    // else{
-    //     req.body.default = false
-    // }
-    // if(req.body.parent == null || req.body.parent == 'none'){
-    //     req.body.parent = 'none'
-    // }
-    // else{
-    //     req.body.default = false
-    // }
-    // if(req.body.replies == null || req.body.replies != false){
-    //     req.body.replies = true
-    // }
-    // else{
-    //     req.body.default = false
-    // }
-    // if(req.body.hasMedia == null || req.body.hasMedia == false){
-    //     req.body.hasMedia = false
-    // }
-    // else{
-    //     req.body.default = false
-    // }
-
-    // //query
-    // req.body.query = {'timestamp':{$lt:req.body.timestamp*1000}}
-    // if (req.body.q != null && req.body.q != "") {
-    //     req.body.query.$text = {$search: req.body.q}
-    //     req.body.default = false
-    // }
-    // if(req.body.username!=null&&req.body.username!=''){
-    //     req.body.query.username = req.body.username
-    //     req.body.default = false
-    // }
-    // else if(req.body.following == true){
-    //     req.app.locals.db.collection("follow").find({'follower':req.session.user}).toArray(function(err, result){
-    //         if(err){
-    //             console.log(err)
-    //             return res.status(500).json({
-    //                 status:"error",
-    //                 error:err
-    //             });
-    //         }
-    //         else{
-    //             req.body.query.username = {$in:result}
-    //         }
-    //     })
-    // }
-    // if(req.body.replies == false){
-    //     req.body.query.parent = {$ne:'reply'}
-    // }
-    // else{
-    //     if(req.body.parent!=null && req.body.parent != 'none' && req.body.parent != ''){
-    //         req.body.query.parent = req.body.parent
-    //     }
-    // }
-    // if(req.body.hasMedia){
-    //     req.body.query.media = {$ne:[]}
-    // }
-    // itemSearch(req,res)
     else{
         req.body.key = 'd'
         //Default values
@@ -177,6 +97,7 @@ router.post('/',(req,res)=>{
             req.body.query.media = {$ne:[]}
         }
         req.body.current_user = req.session.user
+        console.log(req.body)
         request({  
             url: "http://192.168.122.28/search",
             method: 'POST',
