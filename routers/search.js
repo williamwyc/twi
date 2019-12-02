5617289f9723795e5433b23466b7af5595a9d8ec
Yichun Wu <kobo1998@live.com>
2019-12-02 00:07:17 -0500

    M4 search

--- routers/search.js
+++ routers/search.js
@@ -23,65 +23,61 @@ router.post('/',(req,res)=>{
             });
         }
     }
-
+    req.body.default = true
     //Default values
     req.body.current = Date.now()
     if(req.body.timestamp == null || req.body.timestamp == '' || req.body.timestamp <= 0){
         req.body.timestamp = req.body.current
-        req.body.key = 'd'
     }
     else{
-        req.body.key = '123'
+        req.body.default = false
     }
     if(req.body.limit == null || req.body.limit == '' ||parseInt(req.body.limit) <= 0){
         req.body.limit = 25
-        req.body.key += '25'
     }
     else if(parseInt(req.body.limit) >= 100){
         req.body.limit = 100
-        req.body.key += '100'
+        req.body.default = false
     }
     else{
-        req.body.key += req.body.limit
+        req.body.default = false
     }
     if(req.body.rank == null || req.body.rank != 'time'){
         req.body.rank = 'interest'
-        req.body.key += 'i'
     }
     else{
-        req.body.key += 't'
+        req.body.default = false
     }
     if(req.body.parent == null || req.body.parent == 'none'){
         req.body.parent = 'none'
-        req.body.key += 'n'
     }
     else{
-        req.body.key += req.body.parent
+        req.body.default = false
     }
     if(req.body.replies == null || req.body.replies != false){
         req.body.replies = true
-        req.body.key += 'r'
+    }
+    else{
+        req.body.default = false
     }
     if(req.body.hasMedia == null || req.body.hasMedia == false){
         req.body.hasMedia = false
     }
     else{
-        req.body.key += 'm'
+        req.body.default = false
     }
 
     //query
     req.body.query = {'timestamp':{$lt:req.body.timestamp*1000}}
     if (req.body.q != null && req.body.q != "") {
         req.body.query.$text = {$search: req.body.q}
-        req.body.key += req.body.q
+        req.body.default = false
     }
     if(req.body.username!=null&&req.body.username!=''){
         req.body.query.username = req.body.username
-        req.body.key += req.body.username
+        req.body.default = false
     }
     else if(req.body.following == true){
-        req.body.key += 'f'
-        req.body.key += req.session.user
         req.app.locals.db.collection("follow").find({'follower':req.session.user}).toArray(function(err, result){
             if(err){
                 console.log(err)
@@ -105,15 +101,12 @@ router.post('/',(req,res)=>{
     }
     if(req.body.hasMedia){
         req.body.query.media = {$ne:[]}
-        req.body.key += 'm'
     }
     itemSearch(req,res)
 });
 
 function itemSearch(req,res){
-    req.body.key = req.body.key.replace(/\t/g,';').replace(/ /g,';').replace(/\n/g,';')
-    console.log(req.body.key)
-    req.app.locals.mem.get(req.body.key,function(err,data){
+    req.app.locals.mem.get('default',function(err,data){
         if(err){
             console.log(err)
             res.status(500).json({
@@ -142,21 +135,29 @@ function itemSearch(req,res){
                             return (b.property.likes+b.retweeted)/(req.body.current-b.timestamp) - (a.property.likes+a.retweeted)/(req.body.current-a.timestamp)
                         })
                     }
-                    req.app.locals.mem.set(req.body.key,result,100,function(err){
-                        if(err){
-                            console.log(err)
-                            res.status(500).json({
-                                status:"error",
-                                error:err
-                            });
-                        }
-                        else{
-                            res.status(200).json({
-                                status:"OK",
-                                items:result
-                            });
-                        }
-                    })
+                    if (req.body.default == true){
+                        req.app.locals.mem.set('default',result,100,function(err){
+                            if(err){
+                                console.log(err)
+                                res.status(500).json({
+                                    status:"error",
+                                    error:err
+                                });
+                            }
+                            else{
+                                res.status(200).json({
+                                    status:"OK",
+                                    items:result
+                                });
+                            }
+                        })
+                    }
+                    else{
+                        res.status(200).json({
+                            status:"OK",
+                            items:result
+                        });
+                    }
                 }
             })
         }
