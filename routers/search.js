960d6496e241fd2ab46ff99a5734cc89bd7db57f
Yichun Wu <kobo1998@live.com>
2019-12-02 01:32:22 -0500

    M4: microservice

--- routers/search.js
+++ routers/search.js
@@ -5,104 +5,120 @@ var path = require('path');
 var urlencodedParser = bodyParser.urlencoded({extended: false})
 var jsonParser = bodyParser.json()
 var MongoClient = require('mongodb').MongoClient;
+var request = require('request');
 
 router.post('/',(req,res)=>{
-    if(req.body.following == null){
-        if(req.session.user == null){
-            res.status(400).json({
-                status:"error",
-                error:"Login First"
-            });
-        }
-    }
-    else if(req.body.following == true){
-        if(req.session.user == null){
-            res.status(400).json({
-                status:"error",
-                error:"Login First"
-            });
-        }
-    }
-    req.body.default = true
-    //Default values
-    req.body.current = Date.now()
-    if(req.body.timestamp == null || req.body.timestamp == '' || req.body.timestamp <= 0){
-        req.body.timestamp = req.body.current
-    }
-    else{
-        req.body.default = false
-    }
-    if(req.body.limit == null || req.body.limit == '' ||parseInt(req.body.limit) <= 0){
-        req.body.limit = 25
-    }
-    else if(parseInt(req.body.limit) >= 100){
-        req.body.limit = 100
-        req.body.default = false
-    }
-    else{
-        req.body.default = false
-    }
-    if(req.body.rank == null || req.body.rank != 'time'){
-        req.body.rank = 'interest'
-    }
-    else{
-        req.body.default = false
-    }
-    if(req.body.parent == null || req.body.parent == 'none'){
-        req.body.parent = 'none'
-    }
-    else{
-        req.body.default = false
-    }
-    if(req.body.replies == null || req.body.replies != false){
-        req.body.replies = true
-    }
-    else{
-        req.body.default = false
-    }
-    if(req.body.hasMedia == null || req.body.hasMedia == false){
-        req.body.hasMedia = false
-    }
-    else{
-        req.body.default = false
-    }
+    // if(req.body.following == null){
+    //     if(req.session.user == null){
+    //         res.status(400).json({
+    //             status:"error",
+    //             error:"Login First"
+    //         });
+    //     }
+    // }
+    // else if(req.body.following == true){
+    //     if(req.session.user == null){
+    //         res.status(400).json({
+    //             status:"error",
+    //             error:"Login First"
+    //         });
+    //     }
+    // }
+    // req.body.default = true
+    // //Default values
+    // req.body.current = Date.now()
+    // if(req.body.timestamp == null || req.body.timestamp == '' || req.body.timestamp <= 0){
+    //     req.body.timestamp = req.body.current
+    // }
+    // else{
+    //     req.body.default = false
+    // }
+    // if(req.body.limit == null || req.body.limit == '' ||parseInt(req.body.limit) <= 0){
+    //     req.body.limit = 25
+    // }
+    // else if(parseInt(req.body.limit) >= 100){
+    //     req.body.limit = 100
+    //     req.body.default = false
+    // }
+    // else{
+    //     req.body.default = false
+    // }
+    // if(req.body.rank == null || req.body.rank != 'time'){
+    //     req.body.rank = 'interest'
+    // }
+    // else{
+    //     req.body.default = false
+    // }
+    // if(req.body.parent == null || req.body.parent == 'none'){
+    //     req.body.parent = 'none'
+    // }
+    // else{
+    //     req.body.default = false
+    // }
+    // if(req.body.replies == null || req.body.replies != false){
+    //     req.body.replies = true
+    // }
+    // else{
+    //     req.body.default = false
+    // }
+    // if(req.body.hasMedia == null || req.body.hasMedia == false){
+    //     req.body.hasMedia = false
+    // }
+    // else{
+    //     req.body.default = false
+    // }
 
-    //query
-    req.body.query = {'timestamp':{$lt:req.body.timestamp*1000}}
-    if (req.body.q != null && req.body.q != "") {
-        req.body.query.$text = {$search: req.body.q}
-        req.body.default = false
-    }
-    if(req.body.username!=null&&req.body.username!=''){
-        req.body.query.username = req.body.username
-        req.body.default = false
-    }
-    else if(req.body.following == true){
-        req.app.locals.db.collection("follow").find({'follower':req.session.user}).toArray(function(err, result){
-            if(err){
-                console.log(err)
-                return res.status(500).json({
-                    status:"error",
-                    error:err
-                });
-            }
-            else{
-                req.body.query.username = {$in:result}
-            }
-        })
-    }
-    if(req.body.replies == false){
-        req.body.query.parent = {$ne:'reply'}
-    }
-    else{
-        if(req.body.parent!=null && req.body.parent != 'none' && req.body.parent != ''){
-            req.body.query.parent = req.body.parent
+    // //query
+    // req.body.query = {'timestamp':{$lt:req.body.timestamp*1000}}
+    // if (req.body.q != null && req.body.q != "") {
+    //     req.body.query.$text = {$search: req.body.q}
+    //     req.body.default = false
+    // }
+    // if(req.body.username!=null&&req.body.username!=''){
+    //     req.body.query.username = req.body.username
+    //     req.body.default = false
+    // }
+    // else if(req.body.following == true){
+    //     req.app.locals.db.collection("follow").find({'follower':req.session.user}).toArray(function(err, result){
+    //         if(err){
+    //             console.log(err)
+    //             return res.status(500).json({
+    //                 status:"error",
+    //                 error:err
+    //             });
+    //         }
+    //         else{
+    //             req.body.query.username = {$in:result}
+    //         }
+    //     })
+    // }
+    // if(req.body.replies == false){
+    //     req.body.query.parent = {$ne:'reply'}
+    // }
+    // else{
+    //     if(req.body.parent!=null && req.body.parent != 'none' && req.body.parent != ''){
+    //         req.body.query.parent = req.body.parent
+    //     }
+    // }
+    // if(req.body.hasMedia){
+    //     req.body.query.media = {$ne:[]}
+    // }
+    // itemSearch(req,res)
+    request({  
+        url: "http://192.168.122.28/search",
+        method: 'POST',
+        json: data
+    }, 
+    function(err, response, body) {  
+        if(err){
+            console.log(err);
         }
-    }
-    if(req.body.hasMedia){
-        req.body.query.media = {$ne:[]}
-    }
-    itemSearch(req,res)
+        else if(body.status=='error'){
+            res.status(404).json(body);
+        }else{
+            res.json(body);
+        }
+    });
 });
 
 function itemSearch(req,res){
