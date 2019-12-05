1ef17a0f554fcb00abb96438d7c8badd55660830
Yichun Wu <kobo1998@live.com>
2019-12-04 19:35:28 -0500

    Update user.js

--- routers/user.js
+++ routers/user.js
@@ -92,7 +92,7 @@ router.get('/:username/posts',jsonParser,function(req,res){
 
     // })
     request({  
-        url: "http://192.168.122.33/"+req.params.username+'/posts',
+        url: "http://152.44.32.147/"+req.params.username+'/posts',
         method: 'GET',
         json: req.body
     }, 
